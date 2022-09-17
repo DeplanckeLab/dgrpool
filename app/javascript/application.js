1faae1274f67a9ea46439a6d74dbb77c976243ef
Fabrice David <fabrice.david@epfl.ch>
2022-09-17 08:17:09 +0200

    add jquery-ui and fix datatables import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import './add_jquery'
 
+import './src/jquery'
+import './src/jquery-ui'
+import './src/datatables'
